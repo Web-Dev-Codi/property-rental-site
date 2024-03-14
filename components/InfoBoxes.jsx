7dc94c3e8fcf34ed4fdd920ac95c2fb6cac896b7
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-03-14 22:57:47 +0100

    Added Hero, Properies Card and listings

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -16,6 +16,18 @@ const InfoBoxes = () => {
             Find your dream rental property. Bookmark properties and contact
             owners.
           </InfoBox>
+          <InfoBox
+            heading="For Renters"
+            backgroundColor="bg-blue-100"
+            buttonInfo={{
+              text: "Add Properties",
+              link: "/properties/add",
+              backgroundColor: "bg-blue-500",
+            }}
+          >
+            List your properties and reach potential tenants. Rent as an airbnb
+            or long term.
+          </InfoBox>
         </div>
       </div>
     </section>
