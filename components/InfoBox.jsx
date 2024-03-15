929b63fa27b28ffc3fe1a8ad95a1f10e7c62c50c
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-03-15 14:07:50 +0100

    Added Recent Properties

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -7,8 +7,8 @@ const InfoBox = ({
 }) => {
   return (
     <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-      <h2 className={`${textColor} font-bold`}>{heading}</h2>
-      <p className={`${textColor}mt-2 mb-4`}>{children}</p>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{children}</p>
       <a
         href={buttonInfo.link}
         className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2 hover:opacity-80`}
@@ -18,5 +18,4 @@ const InfoBox = ({
     </div>
   );
 };
-
 export default InfoBox;
