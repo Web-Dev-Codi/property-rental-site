477ff1e7fab33a555c124ad957bfb50f31d86e7d
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-03-19 07:32:50 +0100

    Successful login logout with Next-Auth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -147,7 +147,7 @@ const Navbar = () => {
                     />
                   </svg>
                 </button>
-                <UnreadMessageCount session={session} />
+                {/* <UnreadMessageCount session={session} /> */}
               </Link>
               {/* <!-- Profile dropdown button --> */}
               <div className="relative ml-3">
@@ -265,6 +265,8 @@ const Navbar = () => {
                   key={index}
                   className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
+                  <FaGoogle className="text-white mr-2" />
+
                   <span>Login or Register</span>
                 </button>
               ))}
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database.js";
+import User from "@/models/User.js";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -18,15 +20,31 @@ export const authOptions = {
     // Called on successful signin
     async signIn({ account, profile }) {
       // 1. Connect to the DB
+      await connectDB();
       // 2. Check if usere already exists
+      const userExists = await User.findOne({ email: profile.email });
       // 3. if not add to DB
+      if (!userExists) {
+        // Truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4. Return true to allow sign in
+      return true;
     },
     // Modifies the session object
     async session({ session }) {
       // 1. Get user from DB
+      const user = await User.findOne({ email: session.user.email });
       // 2. Assign session id
+      session.user.id = user._id.toString();
       // 3. return that session
+      return session;
     },
   },
 };
