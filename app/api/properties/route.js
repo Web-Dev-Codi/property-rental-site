e9bc246f90f7a2caa173db1ef63490ee160d31ae
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-03-31 20:10:24 +0200

    form complete

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database.js";
 import Property from "@/models/Property";
+import { getServerSession } from "next-auth/next";
 
 // Get /api/properties
 export const GET = async (request) => {
@@ -23,9 +24,38 @@ export const POST = async (request) => {
     const amentities = formData.getAll("amenities");
     const images = formData
       .getAll("images")
-      .filter((image) => images.name !== "");
+      .filter((image) => image.name !== "");
 
-    console.log(amentities, images);
+    // Create Property object for database
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amentities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      images,
+    };
+
+    console.log(propertyData);
+
     return new Response(JSON.stringify({ message: "Success" }));
   } catch (error) {
     return new Response("Failed to add property", { status: 500 });
